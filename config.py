1faf51392497d3564bdd099663dd1e3e3314c82c
Papa_D <zibezt@mail.ru>
2025-09-09 07:27:47 +0800

    написаны конфигурации, загружены зависимости, создан requirements

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    open_api_key: str = os.getenv("OPEN_API_KEY", "")
+    open_base_url: str = os.getenv("OPEN_BASE_URL", "")
+    croma_db_path: str = os.getenv("CHROMA_DB_PATH", "")
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()

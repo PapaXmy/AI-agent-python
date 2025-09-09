2b7b5f2ef0d53b8a8b5817b3374f98259640cdfc
Papa_D <zibezt@mail.ru>
2025-09-09 21:53:19 +0800

    написана функция запроса к LLM с контекстом

---
+++ qa_system.py
@@ -0,0 +1,45 @@
+from langchain.chain import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain_community.llm import HuggingFaceHub
+from langchain_openai import ChatOpenAI
+
+from config import settings
+from vectore_store import get_vector_store
+
+
+def init_qa(use_advanced_llm: bool = True, model_name="gpt-5"):
+    vectordb = get_vector_store()
+    retriever = vectordb.as_retriever()
+
+    if use_advanced_llm:
+        llm = ChatOpenAI(
+            model=model_name,
+            temperature=0.1,
+            max_tokens=2000,
+            open_api_key=settings.open_api_key,
+            open_api_base=settings.open_base_url,
+        )
+    else:
+        llm = HuggingFaceHub(
+            repo_id="google/flan-t5-large",
+            model_kwargs={"temperature": 0.1, "max_length": 1000},
+        )
+
+    prompt_template = """Ты - AI-Python разработчик. Используй контекст, 
+    чтобы отвечать максимально полезно.
+
+    Контекст:
+    {context}
+
+    Вопрос:
+    {question}
+
+    Ответ:"""
+
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["contex", "question"]
+    )
+
+    return RetrievalQA.from_chain_type(
+        llm=llm, retriever=retriever, chain_type_kwargs={"prompt": prompt}
+    )

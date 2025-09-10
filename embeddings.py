0871d3bba9ad855bc193b38c7e9a137e1171729a
Papa_D <zibezt@mail.ru>
2025-09-11 07:40:51 +0800

    не работала инициализация эмбеддингов переписал заново

--- embeddings.py
+++ embeddings.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 from langchain.embeddings import SentenceTransformerEmbeddings
 
@@ -8,14 +8,22 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from config import settings
 
 
-def get_embeddings(provider: str = "openai", model_name: str = "all-MiniLM-L6-v2"):
+def get_embeddings(use_openai: bool = True, model_name: str = "all-MiniLM-L6-v2"):
     """Инициализация эмбеддингов для Chroma"""
-    if provider == "openai":
-        os.environ["OPENAI_API_KEY"] = settings.openai_api_key
-        if settings.openai_base_url:
-            os.environ["OPENAI_API_BASE"] = settings.openai_base_url
+    if use_openai:
+        if not settings.openai_api_key:
+            raise ValueError("Нет ключа OpenAI API, проверте файл .env")
 
-        embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+        openai_kwargs = {
+            "model": "text-embeddings-ada-002",
+            "openai_api_key": settings.openai_api_key,
+        }
+
+        if settings.openai_base_url:
+            openai_kwargs["openai_base_url"] = settings.openai_base_url
+
+        embeddings = OpenAIEmbeddings(**openai_kwargs)
     else:
         embeddings = SentenceTransformerEmbeddings(model_name=model_name)
+
     return embeddings

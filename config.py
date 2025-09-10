df832e4948345df3983565691892f255f440909b
Papa_D <zibezt@mail.ru>
2025-09-11 07:28:15 +0800

    проблемы при merge, config подправил в develop

--- config.py
+++ config.py
@@ -5,25 +5,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-<<<<<<< HEAD
-    openai_api_key: str
-    openai_base_url: str | None = None
-=======
-    open_api_key: str = ""
-    open_base_url: str = ""
->>>>>>> config/fix
+    openai_api_key: str = ""
+    openai_base_url: str = ""
     chroma_db_path: str = "./chroma_db"
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-<<<<<<< HEAD
-=======
         extra = "allow"
 
 
 Path("./chroma_db").mkdir(exist_ok=True)
->>>>>>> config/fix
 
 
 settings = Settings()

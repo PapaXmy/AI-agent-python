194c2a287c1f439a59d72ffefc730b4d0b235a9e
Papa_D <zibezt@mail.ru>
2025-09-10 21:35:19 +0800

    переписана загрузка эмбеддингов в базу данных

--- embeddings.py
+++ embeddings.py
@@ -1,14 +1,21 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_openai import OpenAIEmbeddings
+import os
+
+from langchain.embeddings import SentenceTransformerEmbeddings
+
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai.embeddings import OpenAIEmbeddings
 
 from config import settings
 
 
-def get_embeddings(use_openai: bool = True, model_name: str = "all-MiniLM-L6-v2"):
-    """Возвращает объект эмбеддингов"""
-    if use_openai:
-        return OpenAIEmbeddings(
-            open_api_key=settings.open_api_key, open_api_base=settings.open_base_url
-        )
+def get_embeddings(provider: str = "openai", model_name: str = "all-MiniLM-L6-v2"):
+    """Инициализация эмбеддингов для Chroma"""
+    if provider == "openai":
+        os.environ["OPEN_API_KEY"] = settings.open_api_key
+        if settings.open_base_url:
+            os.environ["OPEN_API_BASE"] = settings.open_base_url
+
+        embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
     else:
-        return HuggingFaceEmbeddings(model_name=model_name)
+        embeddings = SentenceTransformerEmbeddings(model_name=model_name)
+    return embeddings

c732fce0e626a07517f8d8f99830d24c4c62ef59
Papa_D <zibezt@mail.ru>
2025-09-11 07:18:55 +0800

    переработан файл конфигурации, добавлено создание папки для базы данных если ее не существует

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
-import os
+# import os
+from pathlib import Path
 
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    open_api_key: str = os.getenv("open_api_key", "")
-    open_base_url: str = os.getenv("open_base_url", "")
-    chroma_db_path: str = os.getenv("chroma_db_path", "")
+    open_api_key: str = ""
+    open_base_url: str = ""
+    chroma_db_path: str = "./chroma_db"
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "allow"
+
+
+Path("./chroma_db").mkdir(exist_ok=True)
 
 
 settings = Settings()

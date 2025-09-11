07180aab6372b4213dd08e7e7b7317fa812b242c
Papa_D <zibezt@mail.ru>
2025-09-11 08:16:30 +0800

    полностью переписана функция загрузки базы данных, добавлены описания функций

--- vector_store.py
+++ vector_store.py
@@ -1,21 +1,30 @@
 from langchain.vectorstores import Chroma
 
-import embeddings
 from config import settings
 from embeddings import get_embeddings
 
 
-def get_vector_store(provider: str = "openai"):
-    embeddings = get_embeddings(provider)
-    vectordb = Chroma(
-        collection_name="documents",
-        embeddings_function=embeddings,
-        persist_directory=settings.chroma_db_path,
-    )
+def get_vector_store(documents=None, provider: str = "openai"):
+    """Создает или загружает векторное хранилище"""
+    use_openai = provider == "openai"
+    embeddings = get_embeddings(use_openai=use_openai)
+
+    if documents:
+        vectordb = Chroma.from_documents(
+            documents=documents,
+            embeddings=embeddings,
+            persist_directory=settings.chroma_db_path,
+        )
+    else:
+        vectordb = Chroma(
+            persist_directory=settings.chroma_db_path, embeddings_functions=embeddings
+        )
+
     return vectordb
 
 
 def add_documents(docs, provider: str = "openai"):
+    """Добавляет документы в существующее векторное хранилище"""
     vectordb = get_vector_store(provider)
     vectordb.add_documents(docs)
     vectordb.persist()

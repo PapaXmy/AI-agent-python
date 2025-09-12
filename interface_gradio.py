ac91137f8b05f1b5927fee1e0da0320057891c5b
Papa_D <zibezt@mail.ru>
2025-09-12 08:12:17 +0800

    добавлен интерфейс для выбора коллекции документации в базе данных

--- interface_gradio.py
+++ interface_gradio.py
@@ -1,5 +1,9 @@
 import gradio as gr
 
+import vector_store
+from qa_system import init_qa
+from vector_store import get_vector_store, list_project
+
 
 def chat(query, qa_chain):
     """Функция для обработки запросов через Gradio интерфейс"""
@@ -10,14 +14,39 @@ def chat(query, qa_chain):
         return f"Ошибка: {str(e)}"
 
 
-def launch_interface(qa_chain):
+def launch_interface(qa_chain, project_name):
     """Функция которая запускает Gradio интерфейс"""
     iface = gr.Interface(
         fn=lambda query: chat(query, qa_chain),
         inputs="text",
         outputs="text",
-        title="AI Python Agent",
+        title=f"AI Python Agent - коллекция: {project_name}",
         description="Задайте вопрос о вашей документации",
+    )
+
+    return iface.launch(server_name="0.0.0.0", server_port=7860, share=True)
+
+
+def project_selection_interface():
+    """Интерфейс для выбора коллекции"""
+    projects = list_project()
+
+    def load_project(project_name):
+        if project_name and project_name != "Новая коллекция":
+            vector_db = get_vector_store(project_name=project_name)
+            qa_chain = init_qa(vector_db)
+            return launch_interface(qa_chain, project_name)
+        else:
+            return "Пожалуйста, выберите существующий проект или создайте новый через командную строку"
+
+    iface = gr.Interface(
+        fn=load_project,
+        inputs=gr.Dropdown(
+            choice=projects + ["Новая коллекция"], label="Выберете коллекцию"
+        ),
+        outputs="text",
+        title="Выбор коллекции",
+        description="Выберете коллекцию для работы или создайте новую через командную строку",
     )
 
     return iface.launch(server_name="0.0.0.0", server_port=7860, share=True)

6cf625c389d191b1ea690c3abc1c9a8121488572
Papa_D <zibezt@mail.ru>
2025-09-12 19:47:22 +0800

    исправлена инициализация языковой модели для ответа агента

--- qa_system.py
+++ qa_system.py
@@ -2,29 +2,38 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import HuggingFaceHub
 from langchain_openai import ChatOpenAI
+from openai import OpenAI
 
 from config import settings
 from vector_store import get_vector_store
 
 
-def init_qa(vector_db, use_advanced_llm: bool = False, model_name="gpt-4o"):
+def init_qa(vector_db, use_advanced_llm: bool = True, model_name="gpt-4o"):
     retriever = vector_db.as_retriever()
 
     if use_advanced_llm:
         if not settings.openai_api_key:
             raise ValueError("Не найден OpenAI API ключ, проверте ваш .env файл")
 
-        openai_kwargs = {
-            "model": model_name,
-            "temperature": 0.1,
-            "max_tokens": 2000,
-            "openai_api_key": settings.openai_api_key,
-        }
+        client = OpenAI(
+            api_key=settings.openai_api_key, base_url=settings.openai_base_url
+        )
 
-        if settings.openai_base_url:
-            openai_kwargs["openai_base_url"] = settings.openai_base_url
+        llm = ChatOpenAI(
+            client=client, model=model_name, temperature=0.1, max_tokens=2000
+        )
 
-        llm = ChatOpenAI(**openai_kwargs)
+        # openai_kwargs = {
+        #     "model": model_name,
+        #     "temperature": 0.1,
+        #     "max_tokens": 2000,
+        #     "openai_api_key": settings.openai_api_key,
+        # }
+        #
+        # if settings.openai_base_url:
+        #     openai_kwargs["openai_base_url"] = settings.openai_base_url
+        #
+        # llm = ChatOpenAI(**openai_kwargs)
     else:
         llm = HuggingFaceHub(
             repo_id="google/flan-t5-large",

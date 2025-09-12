eb95d229b9ae1c50773b06cb7227ee6b9fe76975
Papa_D <zibezt@mail.ru>
2025-09-12 16:16:05 +0800

    переправлено использование функции get_embeddings для использование локальной модели

--- vector_store.py
+++ vector_store.py
@@ -7,11 +7,14 @@ from embeddings import get_embeddings
 
 
 def get_vector_store(
-    documents=None, provider: str = "openai", project_name: str = "default"
+    documents=None,
+    provider: str = "openai",
+    project_name: str = "default",
+    model_name: str = "BAAI/bge-large-en",
 ):
     """Создает или загружает векторное хранилище"""
     use_openai = provider == "openai"
-    embeddings_functions = get_embeddings(use_openai=use_openai)
+    embeddings_functions = get_embeddings(use_openai=use_openai, model_name=model_name)
     project_path = os.path.join(settings.chroma_db_path, project_name)
     os.makedirs(project_path, exist_ok=True)
     collection_name = f"{project_name}"

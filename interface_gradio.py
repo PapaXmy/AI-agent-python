610b9602ef91e49a8687e1d57b6355a89363cd2b
Papa_D <zibezt@mail.ru>
2025-09-14 21:35:34 +0800

    добавлена история сообщений, переработан интерфейс под чат

--- interface_gradio.py
+++ interface_gradio.py
@@ -8,12 +8,13 @@ from vector_store import get_vector_store, list_project
 logger = logging.getLogger(__name__)
 
 
-def chat(query, qa_chain):
+def chat_with_history(message, history, qa_chain):
     """Функция для обработки запросов через Gradio интерфейс"""
-    logger.info(f"Получен запрос: {query}")
+    logger.info(f"Получен запрос: {message}")
+    logger.info(f"История сообщений: {len(history)} сообщений")
 
     try:
-        result = qa_chain.invoke({"query": query})
+        result = qa_chain.run(message)
 
         if isinstance(result, dict) and "result" in result:
             answer = result["result"]
@@ -27,19 +28,24 @@ def chat(query, qa_chain):
         return error_msg
 
 
-def launch_interface(qa_chain, project_name):
-    """Функция которая запускает Gradio интерфейс"""
+def launch_chat_interface(qa_chain, project_name):
+    """Запуск Gradio интерфейса"""
     logger.info(f"Запуск интерфейса для коллекции: {project_name}")
-    iface = gr.Interface(
-        fn=lambda query: chat(query, qa_chain),
-        inputs="text",
-        outputs="text",
+
+    def predict(message, history):
+        return chat_with_history(message, history, qa_chain)
+
+    chat_interface = gr.ChatInterface(
+        fn=predict,
+        # inputs="text",
+        # outputs="text",
         title=f"AI Python Agent - коллекция: {project_name}",
         description="Задайте вопрос о вашей документации",
+        theme="soft",
     )
     logger.info("Интерфейс Gradio инициализирован!")
 
-    return iface.launch(server_name="0.0.0.0", server_port=7860, share=True)
+    return chat_interface.launch(server_name="0.0.0.0", server_port=7860, share=True)
 
 
 def project_selection_interface():
@@ -57,7 +63,7 @@ def project_selection_interface():
                 vector_db = get_vector_store(project_name=project_name)
                 qa_chain = init_qa(vector_db)
                 logger.info("QA цепочка успешно инициализирована.")
-                return launch_interface(qa_chain, project_name)
+                return launch_chat_interface(qa_chain, project_name)
             except Exception as e:
                 error_msg = f"Ошибка загрузки коллекции: {str(e)}"
                 logger.error(error_msg)

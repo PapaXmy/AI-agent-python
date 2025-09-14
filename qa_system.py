79157f2173d5d33981e77823feb55f4785e274ce
Papa_D <zibezt@mail.ru>
2025-09-14 15:22:37 +0800

    добавлен логгер для системы вопрос ответ для LLM

--- qa_system.py
+++ qa_system.py
@@ -1,3 +1,5 @@
+import logging
+
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import HuggingFaceHub
@@ -5,14 +7,23 @@ from langchain_openai import ChatOpenAI
 
 from config import settings
 
+logger = logging.getLogger(__name__)
+
 
 def init_qa(vector_db, use_advanced_llm: bool = True, model_name="gpt-5"):
+    """Инициализация QA системы (вопрос/ответ LLM)"""
+    logger.info("Инициализация QA системы")
     retriever = vector_db.as_retriever()
+    logger.debug("Retriever создан")
 
     if use_advanced_llm:
-        if not settings.openai_api_key:
-            raise ValueError("Не найден OpenAI API ключ, проверте ваш .env файл")
 
+        if not settings.openai_api_key:
+            error_msg = "Не найден OpenAI API ключ, проверте ваш .env файл"
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+
+        logger.info(f"Использованеи OpenAI модели: {model_name}")
         llm = ChatOpenAI(
             model=model_name,
             temperature=0.1,
@@ -20,8 +31,8 @@ def init_qa(vector_db, use_advanced_llm: bool = True, model_name="gpt-5"):
             openai_api_key=settings.openai_api_key,
             openai_api_base=settings.openai_api_base,
         )
-
     else:
+        logger.info("Использованте локальной модели HuggingFace")
         llm = HuggingFaceHub(
             repo_id="google/flan-t5-large",
             model_kwargs={"temperature": 0.1, "max_length": 1000},
@@ -42,6 +53,7 @@ def init_qa(vector_db, use_advanced_llm: bool = True, model_name="gpt-5"):
         template=prompt_template, input_variables=["context", "question"]
     )
 
+    logger.info("QA система успешно инициализирована")
     return RetrievalQA.from_chain_type(
         llm=llm, retriever=retriever, chain_type_kwargs={"prompt": prompt}
     )
